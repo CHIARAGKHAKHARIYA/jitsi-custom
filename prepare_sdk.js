41172f00b1aa77dfea7b69339d5c17236f0dfb22
CHIARAGKHAKHARIYA <chiragkhakhariya1211@gmail.com>
2024-04-24 19:40:23 +0530

    Fixed  jitsi package issue

--- prepare_sdk.js
+++ prepare_sdk.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const packageJSON = require('../package.json');
+const packageJSON = require('https://github.com/jitsi/jitsi-meet/blob/master/package.json');
 
 const SDKPackageJSON = require('./package.json');
 
@@ -12,245 +12,196 @@ const androidMainTargetPath = './android/src/main/res';
 const iosSrcPath = '../ios/sdk/src';
 const iosDestPath = './ios/src';
 
-
 /**
  * Copies a specified file in a way that recursive copy is possible.
  */
 function copyFileSync(source, target) {
+  let targetFile = target;
 
-    let targetFile = target;
-
-    // If target is a directory, a new file with the same name will be created
-    if (fs.existsSync(target)) {
-        if (fs.lstatSync(target).isDirectory()) {
-            targetFile = path.join(target, path.basename(source));
-        }
+  // If target is a directory, a new file with the same name will be created
+  if (fs.existsSync(target)) {
+    if (fs.lstatSync(target).isDirectory()) {
+      targetFile = path.join(target, path.basename(source));
     }
+  }
 
-    fs.copyFileSync(source, targetFile);
+  fs.copyFileSync(source, targetFile);
 }
-
 
 /**
  * Copies a specified directory recursively.
  */
 function copyFolderRecursiveSync(source, target) {
-    let files = [];
-    const targetFolder = path.join(target, path.basename(source));
+  let files = [];
+  const targetFolder = path.join(target, path.basename(source));
 
-    if (!fs.existsSync(targetFolder)) {
-        fs.mkdirSync(targetFolder, { recursive: true });
-    }
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder, { recursive: true });
+  }
 
-    if (fs.lstatSync(source).isDirectory()) {
-        files = fs.readdirSync(source);
-        files.forEach(file => {
-            const curSource = path.join(source, file);
+  if (fs.lstatSync(source).isDirectory()) {
+    files = fs.readdirSync(source);
+    files.forEach((file) => {
+      const curSource = path.join(source, file);
 
-            if (fs.lstatSync(curSource).isDirectory()) {
-                copyFolderRecursiveSync(curSource, targetFolder);
-            } else {
-                copyFileSync(curSource, targetFolder);
-            }
-        });
-    }
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder);
+      } else {
+        copyFileSync(curSource, targetFolder);
+      }
+    });
+  }
 }
 
 /**
  * Merges the dependency versions from the root package.json with the dependencies of the SDK package.json.
  */
 function mergeDependencyVersions() {
-
-    // Updates SDK dependencies to match project dependencies.
-    for (const key in SDKPackageJSON.dependencies) {
-        if (SDKPackageJSON.dependencies.hasOwnProperty(key)) {
-            SDKPackageJSON.dependencies[key] = packageJSON.dependencies[key] || packageJSON.devDependencies[key];
-        }
+  // Updates SDK dependencies to match project dependencies.
+  for (const key in SDKPackageJSON.dependencies) {
+    if (SDKPackageJSON.dependencies.hasOwnProperty(key)) {
+      SDKPackageJSON.dependencies[key] =
+        packageJSON.dependencies[key] || packageJSON.devDependencies[key];
     }
+  }
 
-    // Updates SDK peer dependencies.
-    for (const key in packageJSON.dependencies) {
-        if (SDKPackageJSON.peerDependencies.hasOwnProperty(key)) {
-
-            // Updates all peer dependencies except react and react-native.
-            if (key !== 'react' && key !== 'react-native') {
-                SDKPackageJSON.peerDependencies[key] = packageJSON.dependencies[key];
-            }
-        }
+  // Updates SDK peer dependencies.
+  for (const key in packageJSON.dependencies) {
+    if (SDKPackageJSON.peerDependencies.hasOwnProperty(key)) {
+      // Updates all peer dependencies except react and react-native.
+      if (key !== 'react' && key !== 'react-native') {
+        SDKPackageJSON.peerDependencies[key] = packageJSON.dependencies[key];
+      }
     }
+  }
 
-    // Updates SDK overrides dependencies.
-    for (const key in packageJSON.overrides) {
-        if (SDKPackageJSON.overrides.hasOwnProperty(key)) {
-            SDKPackageJSON.overrides[key] = packageJSON.overrides[key];
-        }
+  // Updates SDK overrides dependencies.
+  for (const key in packageJSON.overrides) {
+    if (SDKPackageJSON.overrides.hasOwnProperty(key)) {
+      SDKPackageJSON.overrides[key] = packageJSON.overrides[key];
     }
+  }
 
-    const data = JSON.stringify(SDKPackageJSON, null, 4);
+  const data = JSON.stringify(SDKPackageJSON, null, 4);
 
-    fs.writeFileSync('package.json', data);
+  fs.writeFileSync('package.json', data);
 }
 
 // TODO: put this in a seperate step
 mergeDependencyVersions();
 
+copyFolderRecursiveSync('../images', '.');
+copyFolderRecursiveSync('../sounds', '.');
+copyFolderRecursiveSync('../lang', '.');
+copyFolderRecursiveSync('../modules', '.');
+copyFolderRecursiveSync('../react', '.');
+copyFolderRecursiveSync('../service', '.');
+copyFolderRecursiveSync('../ios/sdk/sdk.xcodeproj', './ios');
+copyFolderRecursiveSync(`${iosSrcPath}/callkit`, iosDestPath);
+copyFolderRecursiveSync(`${iosSrcPath}/dropbox`, iosDestPath);
+fs.copyFileSync(`${iosSrcPath}/AppInfo.m`, `${iosDestPath}/AppInfo.m`);
+fs.copyFileSync(`${iosSrcPath}/AudioMode.m`, `${iosDestPath}/AudioMode.m`);
+fs.copyFileSync(
+  `${iosSrcPath}/InfoPlistUtil.m`,
+  `${iosDestPath}/InfoPlistUtil.m`
+);
+fs.copyFileSync(
+  `${iosSrcPath}/InfoPlistUtil.h`,
+  `${iosDestPath}/InfoPlistUtil.h`
+);
+fs.copyFileSync(
+  `${iosSrcPath}/JavaScriptSandbox.m`,
+  `${iosDestPath}/JavaScriptSandbox.m`
+);
+fs.copyFileSync(
+  `${iosSrcPath}/JitsiAudioSession.m`,
+  `${iosDestPath}/JitsiAudioSession.m`
+);
+fs.copyFileSync(
+  `${iosSrcPath}/JitsiAudioSession.h`,
+  `${iosDestPath}/JitsiAudioSession.h`
+);
+fs.copyFileSync(
+  `${iosSrcPath}/JitsiAudioSession+Private.h`,
+  `${iosDestPath}/JitsiAudioSession+Private.h`
+);
+fs.copyFileSync(
+  `${iosSrcPath}/LocaleDetector.m`,
+  `${iosDestPath}/LocaleDetector.m`
+);
+fs.copyFileSync(`${iosSrcPath}/POSIX.m`, `${iosDestPath}/POSIX.m`);
+fs.copyFileSync(`${iosSrcPath}/Proximity.m`, `${iosDestPath}/Proximity.m`);
+copyFolderRecursiveSync(`${androidSourcePath}/log`, `${androidTargetPath}/log`);
 copyFolderRecursiveSync(
-    '../images',
-    '.'
-);
-copyFolderRecursiveSync(
-    '../sounds',
-    '.'
-);
-copyFolderRecursiveSync(
-    '../lang',
-    '.'
-);
-copyFolderRecursiveSync(
-    '../modules',
-    '.'
-);
-copyFolderRecursiveSync(
-    '../react',
-    '.'
-);
-copyFolderRecursiveSync(
-    '../service',
-    '.'
-);
-copyFolderRecursiveSync(
-    '../ios/sdk/sdk.xcodeproj',
-    './ios'
-);
-copyFolderRecursiveSync(
-    `${iosSrcPath}/callkit`,
-    iosDestPath
-);
-copyFolderRecursiveSync(
-    `${iosSrcPath}/dropbox`,
-    iosDestPath
-);
-fs.copyFileSync(
-    `${iosSrcPath}/AppInfo.m`,
-    `${iosDestPath}/AppInfo.m`
-);
-fs.copyFileSync(
-    `${iosSrcPath}/AudioMode.m`,
-    `${iosDestPath}/AudioMode.m`
-);
-fs.copyFileSync(
-    `${iosSrcPath}/InfoPlistUtil.m`,
-    `${iosDestPath}/InfoPlistUtil.m`
-);
-fs.copyFileSync(
-    `${iosSrcPath}/InfoPlistUtil.h`,
-    `${iosDestPath}/InfoPlistUtil.h`
-);
-fs.copyFileSync(
-    `${iosSrcPath}/JavaScriptSandbox.m`,
-    `${iosDestPath}/JavaScriptSandbox.m`
-);
-fs.copyFileSync(
-    `${iosSrcPath}/JitsiAudioSession.m`,
-    `${iosDestPath}/JitsiAudioSession.m`
-);
-fs.copyFileSync(
-    `${iosSrcPath}/JitsiAudioSession.h`,
-    `${iosDestPath}/JitsiAudioSession.h`
-);
-fs.copyFileSync(
-    `${iosSrcPath}/JitsiAudioSession+Private.h`,
-    `${iosDestPath}/JitsiAudioSession+Private.h`
-);
-fs.copyFileSync(
-    `${iosSrcPath}/LocaleDetector.m`,
-    `${iosDestPath}/LocaleDetector.m`
-);
-fs.copyFileSync(
-    `${iosSrcPath}/POSIX.m`,
-    `${iosDestPath}/POSIX.m`
-);
-fs.copyFileSync(
-    `${iosSrcPath}/Proximity.m`,
-    `${iosDestPath}/Proximity.m`
+  `${androidMainSourcePath}/values`,
+  `${androidMainTargetPath}`
 );
 copyFolderRecursiveSync(
-    `${androidSourcePath}/log`,
-     `${androidTargetPath}/log`
+  `${androidMainSourcePath}/drawable-hdpi`,
+  `${androidMainTargetPath}`
 );
 copyFolderRecursiveSync(
-    `${androidMainSourcePath}/values`,
-    `${androidMainTargetPath}`
+  `${androidMainSourcePath}/drawable-mdpi`,
+  `${androidMainTargetPath}`
 );
 copyFolderRecursiveSync(
-    `${androidMainSourcePath}/drawable-hdpi`,
-     `${androidMainTargetPath}`
+  `${androidMainSourcePath}/drawable-xhdpi`,
+  `${androidMainTargetPath}`
 );
 copyFolderRecursiveSync(
-    `${androidMainSourcePath}/drawable-mdpi`,
-     `${androidMainTargetPath}`
+  `${androidMainSourcePath}/drawable-xxhdpi`,
+  `${androidMainTargetPath}`
 );
 copyFolderRecursiveSync(
-    `${androidMainSourcePath}/drawable-xhdpi`,
-     `${androidMainTargetPath}`
+  `${androidMainSourcePath}/drawable-xxxhdpi`,
+  `${androidMainTargetPath}`
 );
-copyFolderRecursiveSync(
-    `${androidMainSourcePath}/drawable-xxhdpi`,
-     `${androidMainTargetPath}`
-);
-copyFolderRecursiveSync(
-    `${androidMainSourcePath}/drawable-xxxhdpi`,
-     `${androidMainTargetPath}`
-);
-copyFolderRecursiveSync(
-    `${androidSourcePath}/net`,
-    `${androidTargetPath}/log`
+copyFolderRecursiveSync(`${androidSourcePath}/net`, `${androidTargetPath}/log`);
+fs.copyFileSync(
+  `${androidSourcePath}/AndroidSettingsModule.java`,
+  `${androidTargetPath}/AndroidSettingsModule.java`
 );
 fs.copyFileSync(
-    `${androidSourcePath}/AndroidSettingsModule.java`,
-    `${androidTargetPath}/AndroidSettingsModule.java`
+  `${androidSourcePath}/AppInfoModule.java`,
+  `${androidTargetPath}/AppInfoModule.java`
 );
 fs.copyFileSync(
-    `${androidSourcePath}/AppInfoModule.java`,
-    `${androidTargetPath}/AppInfoModule.java`
+  `${androidSourcePath}/AudioDeviceHandlerConnectionService.java`,
+  `${androidTargetPath}/AudioDeviceHandlerConnectionService.java`
 );
 fs.copyFileSync(
-    `${androidSourcePath}/AudioDeviceHandlerConnectionService.java`,
-    `${androidTargetPath}/AudioDeviceHandlerConnectionService.java`
+  `${androidSourcePath}/AudioDeviceHandlerGeneric.java`,
+  `${androidTargetPath}/AudioDeviceHandlerGeneric.java`
 );
 fs.copyFileSync(
-    `${androidSourcePath}/AudioDeviceHandlerGeneric.java`,
-    `${androidTargetPath}/AudioDeviceHandlerGeneric.java`
+  `${androidSourcePath}/AudioModeModule.java`,
+  `${androidTargetPath}/AudioModeModule.java`
 );
 fs.copyFileSync(
-    `${androidSourcePath}/AudioModeModule.java`,
-    `${androidTargetPath}/AudioModeModule.java`
+  `${androidSourcePath}/ConnectionService.java`,
+  `${androidTargetPath}/ConnectionService.java`
 );
 fs.copyFileSync(
-    `${androidSourcePath}/ConnectionService.java`,
-    `${androidTargetPath}/ConnectionService.java`
+  `${androidSourcePath}/JavaScriptSandboxModule.java`,
+  `${androidTargetPath}/JavaScriptSandboxModule.java`
 );
 fs.copyFileSync(
-    `${androidSourcePath}/JavaScriptSandboxModule.java`,
-    `${androidTargetPath}/JavaScriptSandboxModule.java`
+  `${androidSourcePath}/LocaleDetector.java`,
+  `${androidTargetPath}/LocaleDetector.java`
 );
 fs.copyFileSync(
-    `${androidSourcePath}/LocaleDetector.java`,
-    `${androidTargetPath}/LocaleDetector.java`
+  `${androidSourcePath}/LogBridgeModule.java`,
+  `${androidTargetPath}/LogBridgeModule.java`
 );
 fs.copyFileSync(
-    `${androidSourcePath}/LogBridgeModule.java`,
-    `${androidTargetPath}/LogBridgeModule.java`
+  `${androidSourcePath}/PictureInPictureModule.java`,
+  `${androidTargetPath}/PictureInPictureModule.java`
 );
 fs.copyFileSync(
-    `${androidSourcePath}/PictureInPictureModule.java`,
-    `${androidTargetPath}/PictureInPictureModule.java`
+  `${androidSourcePath}/ProximityModule.java`,
+  `${androidTargetPath}/ProximityModule.java`
 );
 fs.copyFileSync(
-    `${androidSourcePath}/ProximityModule.java`,
-    `${androidTargetPath}/ProximityModule.java`
-);
-fs.copyFileSync(
-    `${androidSourcePath}/RNConnectionService.java`,
-    `${androidTargetPath}/RNConnectionService.java`
+  `${androidSourcePath}/RNConnectionService.java`,
+  `${androidTargetPath}/RNConnectionService.java`
 );
